50fbb80257e16e1c4810f44683e89aff9baf66a1
doo <doohyun.lee@homeplus.co.kr>
2022-07-27 19:38:17 +0900

    testdome

---
+++ src/main/java/com/example/coding/CodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coding/CodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
